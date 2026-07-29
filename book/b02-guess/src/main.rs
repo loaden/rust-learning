d7233cba6795caf9113a76df744823c1c3901cea
loaden <loaden@live.com>
2026-07-29 10:10:07 +0800

    生成随机数

--- book/b02-guess/src/main.rs
+++ book/b02-guess/src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let _secret_number = rand::thread_rng().gen_range(1..=100);
+
     print!("Please input your guess: ");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read input");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read input");
+    let guess: u32 = guess.trim().parse().expect("Please enter a valid number");
     println!("You guessed: {}", guess);
 }
