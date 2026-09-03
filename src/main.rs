8aa3a39288a0413c65cb626bda2fa02fdc3315e3
loaden <loaden@live.com>
2026-09-03 17:03:43 +0800

    添加所有权规则

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,23 @@ fn main() {
         print!("{},", i);
     }
     println!();
+
+    // 所有权规则
+    println!("所有权规则");
+    /*
+    Rust 中的每一个值都有一个所有者（owner）。
+    在同一时间内，值有且仅有一个所有者。
+    当所有者离开作用域，这个值将被丢弃。
+    Rust 在变量离开作用域时会自动调用 drop 函数来释放资源。
+    Rust 永远不会自动地创建深度拷贝（deep copy）。
+    一旦某种类型实现了 Copy trait，那么它的值就会在赋值时被复制，而不是移动。
+    Copy trait 只适用于那些在栈上分配数据的类型，比如整数类型、浮点数类型、布尔类型和字符类型。
+    Copy trait 不适用于在堆上分配数据的类型，比如 String 和 Vec<T>。
+    实现 Drop trait 的类型将不能实现 Copy trait。
+    如果元组包含的类型都实现了 Copy trait，那么这个元组也会实现 Copy trait。
+    将变量传递给函数将会触发移动（move）或拷贝（copy），具体取决于变量的类型。
+    通过引用（reference）传递变量不会触发移动或拷贝，也叫做借用（borrowing）。
+    对于特定作用域中的特定数据来说，Rust 允许存在唯一可变引用（mutable reference）或者任意数量的不可变引用（immutable reference），但不能同时存在可变引用和不可变引用。
+    引用总是有效的，Rust 编译器会在编译时检查引用的有效性，确保引用不会悬空。
+    */
 }
