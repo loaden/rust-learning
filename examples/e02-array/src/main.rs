ead83b8ec6426310df75497386e05eb7e02d16bf
loaden <loaden@live.com>
2026-09-04 10:07:55 +0800

    数组遍历

--- examples/e02-array/src/main.rs
+++ examples/e02-array/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    let a1 = [1, 2, 3, 4, 5, 6];
-    let mut a2 = ["ok"; 5];
-    a2[0] = "zero";
-    a2.sort();
-    println!("{}", a1.len());
-    println!("{}, {}", a2[0], a2.last().unwrap());
-    for e in a2 {
-        println!("for {}", e);
+    let a = [1, 2, 3, 4, 5, 6];
+    println!("{}", a.len());
+    let mut a = ["ok"; 5];
+    a[0] = "zero";
+    a.sort();
+    println!("{}, {}", a[0], a.last().unwrap());
+    println!("for e in a");
+    for e in a {
+        println!("{:?}", e);
+    }
+    println!("for e in a.iter()");
+    for e in a.iter() {
+        println!("{:?}", e);
+    }
+    println!("for (i, e) in a.iter().enumerate()");
+    for (i, e) in a.iter().enumerate() {
+        println!("{:?}: {:?}", i, e);
     }
 }
