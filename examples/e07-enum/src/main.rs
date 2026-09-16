fb4c50224a206225ae646ebcce6f9474412f8609
loaden <loaden@live.com>
2026-09-16 14:04:32 +0800

    重写enum学习代码

--- examples/e07-enum/src/main.rs
+++ examples/e07-enum/src/main.rs
@@ -3,30 +3,33 @@ fn main() {
     let p = Message::Point { x: 32, y: 66 };
     let t = Message::Title(String::from("title"));
     let c = Message::Color(255, 0, 0);
-    print_enum(&q);
-    print_enum(&p);
-    print_enum(&t);
-    print_enum(&c);
     q.print();
     p.print();
     t.print();
     c.print();
 
     match p {
-        Message::Point { x, y } => println!("Message::Point {}, {}", x, y),
+        Message::Point { x, y } => println!("Message::Point {{ x, y }}  x={},y={}", x, y),
         _ => (),
     }
 
     if let Message::Point { x: 32, y: 66 } = p {
-        println!("Message::Point {:?}", p);
+        println!("if let Message::Point {{ x: 32, y: 66 }} = p");
     }
 
     if let Message::Point { x, y } = p {
-        println!("Message::Point {} - {}", x, y);
+        println!("if let Message::Point {{ x, y }} = p  x={},y={}", x, y);
+    }
+
+    if let Message::Title(s) = t {
+        println!("if let Message::Title(s) = t  s={}", s);
+    }
+
+    if let Message::Color(r, g, b) = c {
+        println!("if let Message::Color(r, g, b) = c  r={},g={},b={}", r, g, b);
     }
 }
 
-#[derive(Debug)]
 enum Message {
     Quit,
     Point { x: i32, y: i32 },
@@ -34,13 +37,13 @@ enum Message {
     Color(i32, i32, i32),
 }
 
-fn print_enum(m: &Message) -> &Message {
-    println!("{:?}", m);
-    m
-}
-
 impl Message {
     fn print(&self) {
-        println!("{:?}", self);
+        match self {
+            Message::Quit => println!("Message::Quit"),
+            Message::Point { x, y } => println!("Message::Point {{ x, y }}  {},{}", x, y),
+            Message::Title(s) => println!("Message::Title(s)  {}", s),
+            Message::Color(r, g, b) => println!("Message::Color(r, g, b)  {},{},{}", r, g, b),
+        }
     }
 }
