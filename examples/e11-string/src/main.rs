b46b62d0ffe3f441d44e93b64ed9d3df95318922
loaden <loaden@live.com>
2026-09-18 15:43:42 +0800

    加深String理解

--- examples/e11-string/src/main.rs
+++ examples/e11-string/src/main.rs
@@ -1,23 +1,23 @@
 fn main() {
-    let s1 = "Hello, world!".to_string();
-    let s2 = String::from("-s2");
+    let s1 = "Hello".to_string();
+    let s2 = String::from("World");
     println!("{}, {}", s1, s2);
 
-    let s3 = s1 + &s2;
-    println!("{}", s3);
+    let s3 = s1 + &s2; // s1 被移动，不能再使用
+    println!("s3 = {}; s2 = {}", s3, s2);
 
-    let mut s4 = "hi".to_string();
+    let mut s4 = "*S4* ".to_string();
     s4 += &s2;
-    println!("{}", s4);
+    println!("s4 = {}, s2 = {}", s4, s2);
 
-    let s5 = format!("{} = {}", s2, s4);
-    println!("{}", s5);
+    let s5 = format!("{} + {}", s2, s4);
+    println!("s5 = {}, s2 = {}", s5, s2);
 
-    let s6 = "中文1英a".to_string();
+    let s6 = "你好Rust".to_string();
     for c in s6.chars() {
-        println!("{}", c);
+        println!("for c in s6.chars = {}", c);
     }
     for b in s6.bytes() {
-        println!("{}", b);
+        println!("for b in s6.bytes = {}", b);
     }
 }
