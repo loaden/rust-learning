800181ee12dace33e1a7052542f84527a482f9b2
loaden <loaden@live.com>
2026-09-22 10:20:26 +0800

    解释枚举和元组

--- syntaxes/enum/src/main.rs
+++ syntaxes/enum/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+    // 枚举用于描述固定数量选项的数据，如性别、大洲、星期、民族、月份等
+    // Rust 允许枚举变体设置整型数值，主要用于和C语言交互，或者在网络传输中节省空间
+    // Rust 允许枚举变体设置结构体，主要用于描述复杂数据结构
     let q = Message::Quit;
     let p = Message::Point { x: 32, y: 66 };
     let t = Message::Title(String::from("title"));
@@ -32,7 +35,7 @@ fn main() {
 
 enum Message {
     Quit,
-    Point { x: i32, y: i32 },
+    Point { x: i32, y: i32 }, // 命名字段，类似结构体
     Title(String),
     Color(i32, i32, i32),
 }
