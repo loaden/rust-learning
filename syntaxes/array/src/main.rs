da0d1e8f491d6a56652cae41356916deb0e23368
loaden <loaden@live.com>
2026-09-22 10:28:22 +0800

    深入数组语法

--- syntaxes/array/src/main.rs
+++ syntaxes/array/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    // 数组是Rust内建的原始集合类型，它表示同类型、长度固定、有序的元素集合。
+    // 数组的类型为[T; N]，其中T表示内部的类型，N表示数组的长度。
+    // 为了保证其编译期可以确定大小的特性，Rust要求N必须在编译期可以求值
+    // 语法：
+    // let arr: [T; N] = [val1, val2, val3...];
+    // let arr: [T; N] = [val; N];
     let a = [1, 2, 3, 4, 5, 6];
     println!("{}", a.len());
     let mut a = ["ok"; 5];
@@ -17,4 +23,16 @@ fn main() {
     for (i, e) in a.iter().enumerate() {
         println!("{:?}: {:?}", i, e);
     }
+
+    // 三种最常见的编译期可以确定值的表达式：字面量、const 常量、CFTE 函数。
+    const fn const_func() -> usize {
+        10
+    }
+    const LEN: usize = 10;
+    let arr = [2026; 10];
+    println!("let arr = [2026; 10] {:?}", arr);
+    let arr = [2026; LEN];
+    println!("let arr = [2026; LEN] {:?}", arr);
+    let arr = [2026; const_func()];
+    println!("let arr = [2026; const_func()] {:?}", arr);
 }
