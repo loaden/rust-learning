22e28ea72335bbc27e83bf9de0f713d6d205f880
loaden <loaden@live.com>
2026-09-22 10:32:49 +0800

    事件枚举示例

--- syntaxes/enum/src/main.rs
+++ syntaxes/enum/src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::E;
+
 fn main() {
     // 枚举用于描述固定数量选项的数据，如性别、大洲、星期、民族、月份等
     // Rust 允许枚举变体设置整型数值，主要用于和C语言交互，或者在网络传输中节省空间
@@ -12,7 +14,9 @@ fn main() {
     c.print();
 
     match p {
-        Message::Point { x, y } => println!("Message::Point {{ x, y }}  x={},y={}", x, y),
+        Message::Point { x, y } => {
+            println!("Message::Point {{ x, y }}  x={},y={}", x, y)
+        }
         _ => (),
     }
 
@@ -29,8 +33,36 @@ fn main() {
     }
 
     if let Message::Color(r, g, b) = c {
-        println!("if let Message::Color(r, g, b) = c  r={},g={},b={}", r, g, b);
+        println!(
+            "if let Message::Color(r, g, b) = c  r={},g={},b={}",
+            r, g, b
+        );
     }
+    // 事件枚举示例
+    println!("事件枚举示例");
+    enum Event {
+        Click,
+        KeyPress(char),
+        Resize { width: u32, height: u32 },
+    }
+    let e = Event::Resize {
+        width: 800,
+        height: 600,
+    };
+    match_event(e);
+    fn match_event(e: Event) {
+        match e {
+            Event::Click => println!("Clicked!"),
+            Event::KeyPress(c) => println!("Key pressed: {}", c),
+            Event::Resize { width, height } => {
+                println!("Resized to {}x{}", width, height);
+            }
+        }
+    }
+    let e = Event::KeyPress('a');
+    match_event(e);
+    let e = Event::Click;
+    match_event(e);
 }
 
 enum Message {
@@ -44,9 +76,13 @@ impl Message {
     fn print(&self) {
         match self {
             Message::Quit => println!("Message::Quit"),
-            Message::Point { x, y } => println!("Message::Point {{ x, y }}  {},{}", x, y),
+            Message::Point { x, y } => {
+                println!("Message::Point {{ x, y }}  {},{}", x, y)
+            }
             Message::Title(s) => println!("Message::Title(s)  {}", s),
-            Message::Color(r, g, b) => println!("Message::Color(r, g, b)  {},{},{}", r, g, b),
+            Message::Color(r, g, b) => {
+                println!("Message::Color(r, g, b)  {},{},{}", r, g, b)
+            }
         }
     }
 }
